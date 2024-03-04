b656fa03ff6f2e602213d708d1a51bd54eecbff3
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2024-03-04 17:03:19 +0100

    WIP - Create User Model

---
+++ src/main/java/com/todolist/project/model/user/User.java
@@ -0,0 +1,17 @@
+package com.todolist.project.model.user;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+public class User implements UserDetails {
+}
